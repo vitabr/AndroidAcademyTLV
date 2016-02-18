d1d125435a4c51e0335156c0fe445a962f30a6f5
Vito Abramov <vito@sensiya.com>
2016-02-18 19:39:05 +0200

    Initial directory structure refactoring

--- app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/MainActivity.java
+++ app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.champions.are.we.androidacademytlv;
+package com.champions.are.we.androidacademytlv.view.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.champions.are.we.androidacademytlv.R;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener {
