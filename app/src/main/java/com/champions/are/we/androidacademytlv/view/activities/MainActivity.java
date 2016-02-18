98ec6181fc19f2a6c81fc27a77f9752f1190f08e
Vito Abramov <vito@sensiya.com>
2016-02-18 20:19:37 +0200

    Splash done

--- app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/MainActivity.java
+++ app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/MainActivity.java
@@ -1,10 +1,11 @@
 package com.champions.are.we.androidacademytlv.view.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,11 +13,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.champions.are.we.androidacademytlv.R;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener {
+
+
+  public static Intent getStartIntent(Context context) {
+    Intent intent = new Intent(context, MainActivity.class);
+    return intent;
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -100,4 +108,5 @@ public class MainActivity extends AppCompatActivity
     drawer.closeDrawer(GravityCompat.START);
     return true;
   }
+
 }
--- app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/SplashActivity.java
+++ app/src/main/java/com/champions/are/we/androidacademytlv/view/activities/SplashActivity.java
@@ -1,19 +1,35 @@
 package com.champions.are.we.androidacademytlv.view.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import com.champions.are.we.androidacademytlv.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by vito on 2/18/2016.
  */
 public class SplashActivity extends Activity {
 
+  private TimerTask mTimerTask = new TimerTask() {
+    @Override
+    public void run() {
+      Intent startIntent = MainActivity.getStartIntent(getBaseContext());
+      startActivity(startIntent);
+      finish();
+    }
+  };
+
   @Override
-  public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-    super.onCreate(savedInstanceState, persistentState);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
+
+    new Timer().schedule(mTimerTask, 3000);
   }
+
+
 }
