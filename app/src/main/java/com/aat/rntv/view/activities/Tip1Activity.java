6d5d93d362b9b3593def8069e869139cceadc14d
vitabr <vitabr@gmail.com>
2016-03-11 10:58:56 +0200

    comit

--- app/src/main/java/com/aat/rntv/view/activities/Tip1Activity.java
+++ app/src/main/java/com/aat/rntv/view/activities/Tip1Activity.java
@@ -3,6 +3,7 @@ package com.aat.rntv.view.activities;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,13 +13,20 @@ public class Tip1Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
         // remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_tip1);
+
+        findViewById(R.id.toolbar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
